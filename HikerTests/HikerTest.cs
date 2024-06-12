51d18b3dcfda62646ddd9fb789600a4cdeaf551e
MartinRetka <martin.retka@tietoevry.com>
2024-06-12 12:45:49 +0200

    Initial commint with tests

---
+++ HikerTests/HikerTest.cs
@@ -0,0 +1,12 @@
+namespace HikerTests;
+
+[TestFixture]
+public class HikerTest
+{
+    [Test]
+    public void life_the_universe_and_everything()
+    {
+        // a simple example to start you off
+        Assert.That(Hiker.Answer(), Is.EqualTo(42));
+    }
+}
